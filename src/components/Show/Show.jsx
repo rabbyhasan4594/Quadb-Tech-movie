3582d5b5fe7c6a85f2b3fc4c3ccb43d2f41369a9
rabbyhasan <rabbyhasan4594@gmail.com>
2023-06-02 14:35:18 +0600

    show added

--- src/components/Show/Show.jsx
+++ src/components/Show/Show.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Button, Card, Container,} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const Show = () => {
+const Show = ({details}) => {
+    console.log(details.show)
+     const show =details.show;
+     console.log(show);
+     const {image,name,id,language,premiered}=show;
     return (
-        <div>
-            
+        <div className='gap-3 mx-lg-auto'>
+           
+           <Card className='lg:mx-auto mb-lg-2' >
+                <Card.Img  variant="top" src={image.original} style={{ height: '350px' }} />
+                <Card.Body>
+                    <Card.Title>Movie Name : {name}</Card.Title>
+                    <div>
+                        <h6>Language : {language} 
+                        </h6>
+                        
+                    </div>
+                    <div>
+                    <h6>Premiered : {premiered} </h6>
+                    </div>
+                    
+                    <Link to={`/showSummary/${id}`}><Button variant="primary">Show Summary</Button></Link>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
